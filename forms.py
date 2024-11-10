7118c1bad75eec2f7e76b5acd9466f80ef1e96b8
Paulo V. <paulocruvel@gmail.com>
2024-11-10 08:54:49 -0300

    Primeiro commit do projeto

---
+++ forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, FileField, DateField, TimeField, SelectField, BooleanField
+from wtforms.validators import DataRequired, length, Email, equal_to
+
+class FormCriarContaPaciente(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired(), length(1, 250)])
+    idade = IntegerField('Idade',  validators=[DataRequired()])
+    datanasc = DateField('Data de nascimento', validators=[DataRequired()])
+    sexo = SelectField('Sexo', choices=[('M', 'Masculino'), ('F', 'Feminino'), ('O', 'Outro')], validators=[DataRequired()])
+    numero = StringField('Número',  validators=[DataRequired()])
+    cpf = StringField('CPF', validators=[DataRequired()])
+    historico = FileField('Histórico médico (opcional)')
+    email = StringField('E-mail', validators=[DataRequired(), Email(), length(255)])
+    senha = PasswordField('Senha', validators=[DataRequired(), length(6, 20)])
+    confirmacao = PasswordField('Confirmação da senha', validators=[DataRequired(), equal_to('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')
+
+class FormLoginPaciente(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
+    botao_submit_login = SubmitField('Fazer Login')
+ 
+class FormCriarContaMedico(FlaskForm):
+    username = StringField('Nome de Usuário', validator=[DataRequired()])
+    idade = IntegerField('Idade', validators=[DataRequired()])
+    datanasc = DateField('Data de nascimento', validators=[DataRequired()])
+    sexo = SelectField('Sexo', choices=[('M', 'Masculino'), ('F', 'Feminino'), ('O', 'Outro')], validators=[DataRequired()])
+    numero = StringField('Número', validators=[DataRequired()])
+    cpf = StringField('CPF', validators=[DataRequired()])
+    crm = StringField('CRM', validators=[DataRequired()]) # CRM + UF + 6 digitos
+    especialidade = StringField('Especialidade', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), length(6, 20)])
+    confirmacao = PasswordField('Confirmação da senha', validators=[DataRequired(), equal_to('senha')])
+    botao_submit_criar_med = SubmitField('Criar Conta')
+
+class FormLoginMedico(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    botao_submit_login_med = SubmitField('Fazer Login')
+
---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for, request, flash, redirect
+from forms import FormCriarContaPaciente, FormLoginPaciente
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e130616980c3ea8d297d37ca384b9260'
+
+
+@app.route('/')
+def landingpage():
+    return render_template('landingpage.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/sobre-nos')
+def sobrenos():
+    return render_template('sobre.html')
+
+@app.route('/login-usuario', methods=['GET', 'POST'])
+def login_usuario():
+    form_login = FormLoginPaciente()
+    form_criarconta = FormCriarContaPaciente()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        # Exibir msg de login bem sucedido
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}.', 'alert-success')
+        # redirecionar para a homepage
+        return redirect(url_for('landingpage'))
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        # Criou conta com sucesso
+        flash(f'Conta criada com sucesso no e-mail: {form_criarconta.email.data}.', 'alert-success')
+        #
+        return redirect(url_for('landingpage'))
+    
+    return render_template('loginusuario.html', form_login=form_login, form_criarconta=form_criarconta)
+
+@app.route('/login-medico', methods=['GET', 'POST'])
+def login_medico():
+    return render_template('loginmed.html')
+
+
+
+if __name__ == ('__main__'):
+    app.run(debug=True)
+ 
