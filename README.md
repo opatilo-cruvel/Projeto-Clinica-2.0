a6cc33fbfa4337a22674f883cba2e75e113db0a9
Paulo Costa <paulohspc@gmail.com>
2025-05-14 19:57:21 -0300

    Adicionando projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+Primeiro projeto desenvolvido na faculdade
--- clinica/__init__.py
+++ clinica/__init__.py
@@ -8,13 +8,19 @@ database = SQLAlchemy()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'e130616980c3ea8d297d37ca384b9260'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2{U|Ny9_\\5sV@localhost:5432/teste'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:paulo890@localhost:5432/teste'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-database.init_app(app)  # Inicializa a instância de database que vem de models.py
+database.init_app(app)  
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_usuario'
 login_manager.login_message_category = 'alert-info'
 
+from .models import Paciente, Medico, Adm, Consultas, Contato   
+with app.app_context():
+    database.create_all()
+
+
 from clinica import routes
+
